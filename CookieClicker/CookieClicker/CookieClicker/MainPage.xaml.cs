d7224703ebc11c640d0f0856b610aab66572be15
Gurvir Singh <charizardzx9@gmail.com>
2020-10-01 07:44:39 -0400

    Made cookie move randomly when pressed

--- CookieClicker/CookieClicker/CookieClicker/MainPage.xaml.cs
+++ CookieClicker/CookieClicker/CookieClicker/MainPage.xaml.cs
@@ -30,8 +30,28 @@ namespace CookieClicker
         private async void PressCookie()
         {
             count++;
-            MyLabel.Text = "Cookie Pressed " + count + " Times!";
-
+            switch (count % 4)
+            {
+                case 0:
+                    MyLabel.Text = "Oh look I'm here now!";
+                    break;
+                case 1:
+                    MyLabel.Text = "Now I'm over here!";
+                    break;
+                case 2:
+                    MyLabel.Text = "And now I'm here!";
+                    break;
+                case 3:
+                    MyLabel.Text = "Can you catch up to me?";
+                    break;
+                default:
+                    MyLabel.Text = "Oh look I'm here now";
+                    break;
+            }
+            var rand = new Random();
+            await Cookie.TranslateTo(rand.Next(-200, 201), rand.Next(-200, 151), easing: Easing.Linear);
+            //Cookie.TranslationX = Cookie.X + rand.Next(-75, 0);
+            //Cookie.TranslationY = Cookie.Y + rand.Next(-75, 0);
             await Cookie.ScaleTo(1.2, 125U, Easing.CubicInOut);
             await Cookie.ScaleTo(1.0, 125U, Easing.CubicInOut);
         }
